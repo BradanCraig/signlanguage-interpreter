9c9f4e8152a6bcbaffdf9416db7ac5f0885a6cd4
Bradan Craig <bradancraig2004@gmail.com>
2024-08-09 15:17:09 -0400

    Basic CSS and connections to other pages

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,16 @@ import "./home.css"
 
 export default function Home(){
     return(
-    <div>
+    <div class="global">
     <h1>AI ASL Interpreter</h1>
-    <p class="description">This application allows you to take or upload images of the ASL alphabet and will use artificial intelligence in order to translate that image</p>
     <Image class="Diagram" src={ASLDiagram} alt="ASL Diagram"></Image>
-    <p class="instruction">For reference, please use the image for reference. As for 'I' and 'J', please replicate the hand position shown on the graphic and ignore the movement aspect of the letters.</p>
+    <p class="description">This application allows you to take or upload images of the ASL alphabet and will use artificial intelligence in order to translate that image</p>
+    <p class="instruction">For reference, please use the image along the side.</p>
+    <p class="instruction">As for 'I' and 'J', please replicate the hand position shown on the graphic and ignore the movement aspect of the letters.</p>
+    <div class="links">
     <Link href="/Live">Take Live Image</Link>
     <Link href="/Upload">Upload Image</Link>
+    </div>
 
     </div>
 )}
