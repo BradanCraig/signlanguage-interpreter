8e76d83deadce1d4df8d89af63e1ac5606d26ae1
Bradan Craig <bradancraig2004@gmail.com>
2024-08-09 13:30:02 -0400

    Update myscript.js

--- src/app/Upload/myscript.js
+++ src/app/Upload/myscript.js
@@ -2,14 +2,18 @@
 const MyInput = () => {
 
     const ImageUploaded = () => {
-            let input = document.getElementById("file")
+            let input = document.getElementById("file") //Get image
             input.onchange = () => {
                 const img = input.files[0]
-                let final= document.getElementById("picture")
+                let final= document.getElementById("picture") //access displayed tag for image
                 final.src = URL.createObjectURL(img)
             }
     }
 
+
+    const SendToModel = (submitedImage) => {
+
+    }
 
 
 return (        
@@ -22,6 +26,7 @@ return (
     <p>Drag and drop images here or <span className ="browse"></span></p>
     <input type="file" id ="file" multiple="multiple" onClick={ImageUploaded}></input>
     <img id="picture" width="900" height="900"></img>
+    <button id="submit" onClick={SendToModel}></button>
 </div>
 
 <div>
