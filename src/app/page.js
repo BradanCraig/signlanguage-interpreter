9d1f0bb43f46053378e34e5d4e3248bceeb61ceb
Bradan Craig <bradancraig2004@gmail.com>
2024-08-21 17:22:18 -0400

    New CSS

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,16 @@ import "./home.css"
 export default function Home(){
     return(
     <div class="global">
-    <h1>AI ASL Interpreter</h1>
-    <Image class="Diagram" src={ASLDiagram} alt="ASL Diagram"></Image>
-    <p class="description">This application allows you to take or upload images of the ASL alphabet and will use artificial intelligence in order to translate that image</p>
-    <p class="instruction">For reference, please use the image along the side.</p>
-    <p class="instruction">As for 'I' and 'J', please replicate the hand position shown on the graphic and ignore the movement aspect of the letters.</p>
-    <div class="links">
-    <Link href="/Live">Take Live Image</Link>
-    <Link href="/Upload">Upload Image</Link>
-    </div>
-
+        <h1>AI ASL Interpreter</h1>
+        <div class="Diagram">
+            <Image class="Diagram"src={ASLDiagram} alt="ASL Diagram"></Image>
+        </div>
+                <p class="description">This application allows you to take or upload images of the ASL alphabet and will use artificial intelligence in order to translate that image</p>
+                <p class="instruction">For reference, please use the image along the side.</p>
+                <p class="instruction">As for 'I' and 'J', please replicate the hand position shown on the graphic and ignore the movement aspect of the letters.</p>
+        <div class="links">
+            <Link href="/Live">Take Live Image</Link>
+            <Link href="/Upload">Upload Image</Link>
+        </div>
     </div>
 )}
