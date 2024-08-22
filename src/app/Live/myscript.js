667bdf3c50d3fa4a0d199cf066e5af37611a311e
Bradan Craig <bradancraig2004@gmail.com>
2024-08-22 12:05:50 -0400

    New CSS

--- src/app/Live/myscript.js
+++ src/app/Live/myscript.js
@@ -4,6 +4,8 @@ import { useEffect, useRef } from 'react';
 
 const MyWebcam = () => {
     const videoRef = useRef(null);
+
+
   useEffect(() => {
     const getUserMedia = async () => {
       try {
@@ -27,23 +29,32 @@ const MyWebcam = () => {
         console.error('Video not ready');
     }
     else{
-    const canvas = document.createElement('canvas');
-    canvas.width=600
-    canvas.length=600
-    const context = canvas.getContext('2d')
-    let con =context.drawImage(videoRef.current, 0, 0, 600, 150);
-    const imgUrl = canvas.toDataURL('image/png');
-    const img= document.getElementById("picture")
-    img.src = imgUrl
+      const canvas = document.createElement('canvas');
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+      const context = canvas.getContext('2d');
+      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+      const imgUrl = canvas.toDataURL('image/png');
+      const img= document.getElementById("picture")
+            img.src = imgUrl
     }
   }
 
   return (
-    <div className="component">
+    /*<div className="component">
       <video ref={videoRef} autoPlay width="900" height="900"></video>
+      <div className="image-container">
+        <img id="picture" width="900" height="900"></img>
+      </div>
       <button type="button" onClick={takePicture}>Take Picture</button>
-      <img id="picture" width="900" height="900"></img>
+    </div>*/
+    <div className="component">
+    <video ref={videoRef} autoPlay width="900" height="900" />
+    <div className="image-container">
+      <img id="picture" width="900" height="900" alt="Captured" />
     </div>
+    <button type="button" onClick={takePicture}>Take Picture</button>
+  </div>
   );
 };
 
